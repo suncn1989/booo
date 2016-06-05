1959005f9672902635a655c7f3d26d63b5c761e7
suncn1989 <nj.s.c.ning@hotmail.com>
2016-06-05 20:29:19 +0800

    Added paging.
    Signed-off-by: suncn1989 <nj.s.c.ning@hotmail.com>

--- js/booking.js
+++ js/booking.js
@@ -1,3 +1,6 @@
+//pic shown on each page
+var pic_count = 8;
+
 //tabs
 $(document).ready(function () 
 {
@@ -14,7 +17,7 @@ $(document).ready(function ()
 		setSelectData($(this).attr("id"));
 	});
 	
-	var pic_count = 4;
+
 	
 	
 	showMassagistList();
@@ -224,13 +227,61 @@ function getJsonData_massagist()
   });
 });
 	*/
-	
+		
 	$.getJSON("./js/staffs.json",function(data)
 	{
+		var pagesnum = getPagesNum(data.staffs.length, pic_count);
+		
+		
+		
+		console.log("pagesnum"+pagesnum);
+		//show photo
 		for (var i=0; i<data.staffs.length; i++)
 		{
 			//$("#massagist_choose").append("<div>" + data.staffs[i].name + "</div>");
 			$("#massagist_choose").append("<div class=\"col-xs-6 col-md-3\"><a href=\"#\" class=\"thumbnail\"><img src=\"" + data.staffs[i].photo + "\" alt=\"技师1\" class=\"massagist_head\"></a></div>");
 		}
+		
+		//show page lists
+		if (data.staffs.length <=3)
+		{
+			if (data.staffs.length == 0)
+			{
+				$("#pagenums").remove();
+			}
+			else
+			{
+				for (var i=0; i<data.staffs.length; i++)
+				{
+					$("#pagenums").append("<li><a href=\"#\">"+ (i+1) + "</a></li>");
+				}
+				
+			}
+		}
+		else
+		{
+			for (var i=0; i<3; i++)
+			{
+				$("#pagenums").append("<li><a href=\"#\">"+ (i+1) + "</a></li>");
+			}
+			$("#pagenums").append("<li><a>...</a></li>");
+			$("#pagenums").append("<li><a href=\"#\">"+ data.staffs.length + "</a></li>");
+		}
+		$("#pagenums").append("<li><a href=\"#\" aria-label=\"Next\"><span aria-hidden=\"true\">&raquo;</span></a></li>");
 	});
+}
+
+function getPagesNum(totalNum, numOnOnepage)
+{
+	/*
+	if(totalNum%numOnOnepage == 0)
+	{
+		return totalNum/numOnOnepage;
+	}
+	else
+	{
+		return totalNum/numOnOnepage+1;
+	}
+	*/
+	return Math.ceil(totalNum/numOnOnepage);
 }
