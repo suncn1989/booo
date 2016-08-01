97de0f2ae5dd6f5ec86936435e609429f289af38
suncn1989 <nj.s.c.ning@hotmail.com>
2016-08-01 19:05:58 +0800

    Added modals
    Signed-off-by: suncn1989 <nj.s.c.ning@hotmail.com>

--- js/booking.js
+++ js/booking.js
@@ -1,5 +1,7 @@
 //pic shown on each page
 var pic_count = 8;
+var curPageNum = 1;
+var pagesnum;
 
 //tabs
 $(document).ready(function () 
@@ -214,10 +216,49 @@ function GetJsonData()
 function showMassagistList()
 {
 	//$("#massagist_choose").append("<div>foagoaemgadokgaqpogoadpgola</div>");
-	getJsonData_massagist();
+	
+	getJsonData_pagelist();
+	getJsonData_massagistPicList(curPageNum);
 }
 
-function getJsonData_massagist()
+function getJsonData_pagelist()
+{
+	$.getJSON("./js/staffs.json",function(data)
+	{
+		pagesnum = getPagesNum(data.staffs.length, pic_count);
+		console.log("pagesnum"+pagesnum);
+		
+		//show page lists
+		if (pagesnum <=3)
+		{
+			if (pagesnum == 0)
+			{
+				$("#pagenums").remove();
+			}
+			else
+			{
+				for (var i=0; i<pagesnum; i++)
+				{
+					console.log("<li><a onclick=\"getJsonData_massagistPicList("+ (i+1) +")\">"+ (i+1) + "</a></li>");
+					$("#pagenums").append("<li><a onclick=\"getJsonData_massagistPicList("+ (i+1) +")\">"+ (i+1) + "</a></li>");
+				}
+				
+			}
+		}
+		else
+		{
+			for (var i=0; i<3; i++)
+			{
+				$("#pagenums").append("<li><a onclick=\"getJsonData_massagistPicList("+ (i+1) +")\">"+ (i+1) + "</a></li>");
+			}
+			$("#pagenums").append("<li><a>...</a></li>");
+			$("#pagenums").append("<li><a onclick=\"getJsonData_massagistPicList("+ (pagesnum) +")\">"+ pagesnum + "</a></li>");
+		}
+		$("#pagenums").append("<li><a aria-label=\"Next\" onclick=\"nextPage()\"><span aria-hidden=\"true\">&raquo;</span></a></li>");
+	});
+}
+
+function getJsonData_massagistPicList(inPageNum)
 {
 	/*
 	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=cat&tagmode=any&format=json&jsoncallback=?", function(data){
@@ -227,61 +268,117 @@ function getJsonData_massagist()
   });
 });
 	*/
-		
+	curPageNum = inPageNum;
 	$.getJSON("./js/staffs.json",function(data)
 	{
-		var pagesnum = getPagesNum(data.staffs.length, pic_count);
-		
-		
-		
-		console.log("pagesnum"+pagesnum);
-		//show photo
-		for (var i=0; i<data.staffs.length; i++)
+		//var pagesnum = getPagesNum(data.staffs.length, pic_count);
+		var beginIndex = (inPageNum-1)*pic_count;
+		var endIndex;
+		if (inPageNum*pic_count > data.staffs.length)
 		{
-			//$("#massagist_choose").append("<div>" + data.staffs[i].name + "</div>");
-			$("#massagist_choose").append("<div class=\"col-xs-6 col-md-3\"><a href=\"#\" class=\"thumbnail\"><img src=\"" + data.staffs[i].photo + "\" alt=\"技师1\" class=\"massagist_head\"></a></div>");
-		}
-		
-		//show page lists
-		if (data.staffs.length <=3)
-		{
-			if (data.staffs.length == 0)
+			if (inPageNum == pagesnum)
 			{
-				$("#pagenums").remove();
+				endIndex = data.staffs.length % pic_count + beginIndex -1;
 			}
 			else
 			{
-				for (var i=0; i<data.staffs.length; i++)
-				{
-					$("#pagenums").append("<li><a href=\"#\">"+ (i+1) + "</a></li>");
-				}
-				
+				endIndex = pic_count-1 + beginIndex;
 			}
 		}
 		else
 		{
-			for (var i=0; i<3; i++)
-			{
-				$("#pagenums").append("<li><a href=\"#\">"+ (i+1) + "</a></li>");
-			}
-			$("#pagenums").append("<li><a>...</a></li>");
-			$("#pagenums").append("<li><a href=\"#\">"+ data.staffs.length + "</a></li>");
+			endIndex = pic_count-1 + beginIndex;
 		}
-		$("#pagenums").append("<li><a href=\"#\" aria-label=\"Next\"><span aria-hidden=\"true\">&raquo;</span></a></li>");
+		//var endIndex = 3
+		//show photo
+		showMassagistPhoto(beginIndex, endIndex, data);
 	});
 }
 
 function getPagesNum(totalNum, numOnOnepage)
 {
-	/*
-	if(totalNum%numOnOnepage == 0)
-	{
-		return totalNum/numOnOnepage;
-	}
-	else
-	{
-		return totalNum/numOnOnepage+1;
-	}
-	*/
 	return Math.ceil(totalNum/numOnOnepage);
+}
+
+function showMassagistPhoto(beginIndex, endIndex, inData)
+{
+	console.log("b:"+beginIndex+"e:"+endIndex);
+	$("#massagist_choose").empty();
+	for (var i=beginIndex; i<=endIndex; i++)
+	{
+		//$("#massagist_choose").append("<div>" + data.staffs[i].name + "</div>");
+		$("#massagist_choose").append("<div class=\"col-xs-6 col-md-3\"><a data-toggle=\"modal\" data-target=\"#Modal_"+ i +"\" class=\"thumbnail\"><img src=\"" + inData.staffs[i].photo + "\" alt=\"技师1\" class=\"massagist_head\"></a></div>");
+		$("#massagist_choose").append("<div class=\"modal fade\" id=\"Modal_" + i +"\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"ModalLabel_" + i + "\">" 
+		+ "<div class=\"modal-dialog\" role=\"document\">"
+		+ "<div class=\"modal-content\">"
+		+ "<div class=\"modal-header\">"
+			+  "<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>"
+			+  "<h4 class=\"modal-title\" id=\"ModalLabel_" + i + "\">技师："+ inData.staffs[i].id +"</h4>"
+		+  "</div>"
+		+  "<div class=\"modal-body\">"
+			+  "<div class=\"row\">"
+				+  "<div class=\"col-md-4 col-md-offset-2\">姓名：</div>"
+				+  "<div class=\"col-md-6 col-md-offest-2\">"+ inData.staffs[i].name +"</div>"
+			+  "</div>"
+			+  "<div class=\"row\">"
+				+  "<div class=\"col-md-4 col-md-offset-2\">生日：</div>"
+				+  "<div class=\"col-md-6 col-md-offest-2\">"+ inData.staffs[i].birthday +"</div>"
+			+  "</div>"
+			+  "<div class=\"row\">"
+				+  "<div class=\"col-md-4 col-md-offset-2\">工作时间：</div>"
+				+  "<div class=\"col-md-6 col-md-offest-2\">"+ inData.staffs[i].workyear +"</div>"
+			+  "</div>"
+			+  "<div class=\"row\">"
+				+  "<div class=\"col-md-4 col-md-offset-2\">电话：</div>"
+				+  "<div class=\"col-md-6 col-md-offest-2\">"+ inData.staffs[i].phone +"</div>"
+			+  "</div>"
+			+  "<div class=\"row\">"
+				+  "<div class=\"col-md-4 col-md-offset-2\">认证编号：</div>"
+				+  "<div class=\"col-md-6 col-md-offest-2\">"+ inData.staffs[i].certno +"</div>"
+			+  "</div>"
+			+  "<div class=\"row\">"
+				+  "<div class=\"col-md-4 col-md-offset-2\">技师介绍：</div>"
+				+  "<div class=\"col-md-6 col-md-offest-2\">"+ inData.staffs[i].duration +"</div>"
+			+  "</div>"
+			+  "<div class=\"row\">"
+				+  "<div class=\"col-md-4 col-md-offset-2\">证书图片：</div>"
+				+  "<div class=\"col-md-6 col-md-offest-2\">"+ inData.staffs[i].certificate +"</div>"
+			+  "</div>"
+			+  "<div class=\"row\">"
+				+  "<div class=\"col-md-4 col-md-offset-2\">技师位置：</div>"
+				+  "<div class=\"col-md-6 col-md-offest-2\">"+ inData.staffs[i].address +"</div>"
+			+  "</div>"
+		+  "</div>"
+		+  "<div class=\"modal-footer\">"
+		+  "<button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>"
+		+  "<button type=\"button\" class=\"btn btn-primary\">Save changes</button>"
+		+  "</div>"
+		+  "</div>"
+		+  "</div>"
+		+  "</div>");
+	}
+}
+
+
+
+
+function prePage()
+{
+	curPageNum--;
+	if (curPageNum < 1)
+	{
+		curPageNum = pagesnum;
+	}
+	getJsonData_massagistPicList(curPageNum);
+	//console.log("pagesnum："+pagesnum);
+}
+
+function nextPage()
+{
+	curPageNum++;
+	if (curPageNum > pagesnum)
+	{
+		curPageNum = 1;
+	}
+	getJsonData_massagistPicList(curPageNum);
 }
